a72ce93888830773a0a2471c47237794e886a91d
WiiruxCruz <hialf.92@gmail.com>
2022-11-06 01:53:36 -0600

    Embedded types

--- src/main/java/com/wiirux/orderService/domain/OrderHeader.java
+++ src/main/java/com/wiirux/orderService/domain/OrderHeader.java
@@ -2,7 +2,10 @@ package com.wiirux.orderService.domain;
 
 import java.util.Objects;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
 import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,11 +13,49 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
+@AttributeOverrides({
+	@AttributeOverride(
+		name = "shippingAddress.address",
+		column = @Column(name = "shipping_address")
+	),
+	@AttributeOverride(
+		name = "shippingAddress.city",
+		column = @Column(name = "shipping_city")
+	),
+	@AttributeOverride(
+		name = "shippingAddress.state",
+		column = @Column(name = "shipping_state")
+	),
+	@AttributeOverride(
+		name = "shippingAddress.zipCode",
+		column = @Column(name = "shipping_zip_code")
+	),
+	@AttributeOverride(
+		name = "billToAddress.address",
+		column = @Column(name = "bill_to_address")
+	),
+	@AttributeOverride(
+		name = "billToAddress.city",
+		column = @Column(name = "bill_to_city")
+	),
+	@AttributeOverride(
+		name = "billToAddress.state",
+		column = @Column(name = "bill_to_state")
+	),
+	@AttributeOverride(
+		name = "billToAddress.zipCode",
+		column = @Column(name = "bill_to_zip_code")
+	)
+})
 public class OrderHeader extends BaseEntity{
 	
 	private String customer;
 	
-	public OrderHeader() {}
+	@Embedded
+	private Address shippingAddress;
+	
+	@Embedded
+	private Address billToAddress;
 	
 	public String getCustomer() {
 		return customer;
@@ -22,18 +63,29 @@ public class OrderHeader extends BaseEntity{
 	public void setCustomer(String customer) {
 		this.customer = customer;
 	}
-
+	public Address getShippingAddress() {
+		return shippingAddress;
+	}
+	public void setShippingAddress(Address shippingAddress) {
+		this.shippingAddress = shippingAddress;
+	}
+	public Address getBillToAddress() {
+		return billToAddress;
+	}
+	public void setBillToAddress(Address billToAddress) {
+		this.billToAddress = billToAddress;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + Objects.hash(customer);
+		result = prime * result + Objects.hash(billToAddress, customer, shippingAddress);
 		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
-		/*
 		if (this == obj)
 			return true;
 		if (!super.equals(obj))
@@ -41,18 +93,7 @@ public class OrderHeader extends BaseEntity{
 		if (getClass() != obj.getClass())
 			return false;
 		OrderHeader other = (OrderHeader) obj;
-		return Objects.equals(customer, other.customer);
-		*/
-		
-		if (this == obj) return true;
-		if (!(obj instanceof OrderHeader)) return false;
-		if (!super.equals(obj)) return false;
-		
-		OrderHeader that = (OrderHeader) obj;
-		
-		return getCustomer() != null ? getCustomer().equals(that.getCustomer()) : that.getCustomer() == null;
-	}
-	
-	
-	
+		return Objects.equals(billToAddress, other.billToAddress) && Objects.equals(customer, other.customer)
+				&& Objects.equals(shippingAddress, other.shippingAddress);
+	}	
 }
