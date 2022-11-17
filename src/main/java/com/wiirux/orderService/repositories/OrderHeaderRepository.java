c0a30344191a7d244b3b66e0f6b7b1aae7fa8ba7
WiiruxCruz <hialf.92@gmail.com>
2022-11-17 03:44:07 -0600

    Posible solucion al no borrado en pruebas

--- src/main/java/com/wiirux/orderService/repositories/OrderHeaderRepository.java
+++ src/main/java/com/wiirux/orderService/repositories/OrderHeaderRepository.java
@@ -1,9 +1,19 @@
 package com.wiirux.orderService.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Query;
 
 import com.wiirux.orderService.domain.OrderHeader;
 
-public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Long> {
+//import jakarta.transaction.Transactional;
 
+
+public interface OrderHeaderRepository extends JpaRepository<OrderHeader, Long> {
+	/*
+	@Modifying
+	@Transactional
+	@Query(value = "delete from OrderHeader OH WHERE OH.id = ?1")
+	void deleteById(Long id);
+	*/
 }
